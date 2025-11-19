2fbe6fc59c860eeb94168f5b9449f0653f058b57
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-19 14:40:54 +0530

    feat(ai-recommend): change productId to productId array

---
+++ backend/src/models/aiRecommendation.model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Model, Document, mongo } from "mongoose";
+
+interface IAiRecommendation extends Document {
+            userId: mongoose.Types.ObjectId;
+            productIds: mongoose.Types.ObjectId[];
+            updatedAt: Date
+}
+
+const aiRecommendationSchema = new Schema<IAiRecommendation>(
+            {
+                        userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+                        productIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }],
+            }, {
+            timestamps: true
+}
+)
+
+
+const aiRecommend: Model<IAiRecommendation> =
+            mongoose.model<IAiRecommendation>("aiRecommend", aiRecommendationSchema);
+
+export default aiRecommend;
