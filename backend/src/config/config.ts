6beddb5a068cca1785172830a7fd7504f4e9b56a
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-19 14:35:43 +0530

    Added configuration

---
+++ backend/src/config/config.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+            PORT: process.env.PORT,
+            NODE_ENV: process.env.NODE_ENV,
+
+            MONGO_URI: process.env.MONGO_URI,
+
+            JWT_SECRET: process.env.JWT_SECRET,
+            JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+            JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES,      // e.g. "15m"
+            JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES,    // e.g. "30d"
+
+            CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+            STRIPE_SECRET: process.env.STRIPE_SECRET,
+            RAPIDAPI_KEY: process.env.RAPIDAPI_KEY,
+            OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+
+            REDIS_URL: process.env.REDIS_URL,
+
+            BASE_URL: process.env.BASE_URL,
+
+            SMTP_HOST: process.env.SMTP_HOST,
+            SMTP_PORT: process.env.SMTP_PORT,
+            SMTP_USER: process.env.SMTP_USER,
+            SMTP_PASS: process.env.SMTP_PASS
+};
