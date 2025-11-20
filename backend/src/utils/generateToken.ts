93fa563cba28e4d9bacea321884bbe470d3793d0
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-20 09:59:35 +0530

    feat: added a token utility functions

---
+++ backend/src/utils/generateToken.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import config from "../config/config";
+
+
+export const generateAccessToken = (userId: string) => {
+            return jwt.sign(
+                        { id: userId },
+                        config.JWT_SECRET as string,
+                        { expiresIn: "15m" }
+            );
+};
+
+
+
+export const generateRefreshToken = (userId: string) => {
+            return jwt.sign(
+                        { id: userId },
+                        config.JWT_REFRESH_SECRET as string,
+                        { expiresIn: "7d" }
+            );
+};
