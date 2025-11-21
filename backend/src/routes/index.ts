6840bb297141847b903880569df17e56cf099de8
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-21 21:28:21 +0530

    feat:added new rouets in index.js

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes"
+import productRoutes from "./productRoutes"
 const router = Router();
 
-router.use("/auth", authRoutes)
-
+router.use("/auth", authRoutes);
+router.use("/product", productRoutes);
 
 export default router;
