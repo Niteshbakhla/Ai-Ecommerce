b44b5c786b4e761ebeeae297de37a73ef969421a
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-21 19:59:47 +0530

    feat: add cookie-parser middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import router from "./routes/index";
+import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
+app.use(cookieParser())
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
