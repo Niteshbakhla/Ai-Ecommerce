c7cd731516ba4d8471dd985c78de97e8b91cd6cd
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-21 20:17:30 +0530

    feat: added refresh route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authController";
+import { login, refresh, register } from "../controllers/authController";
 
 
 const router = Router();
 
-router.route("/register").post(register)
-router.route("/login").post(login)
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/refresh").get(refresh);
 
 
 export default router;
