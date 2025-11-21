f86cbfe28c90dbdf540b747afc6278f706858896
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-21 19:07:10 +0530

    refactor: make some changes in register and login handler

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,25 +2,33 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { registerUser, loginUser, refreshAccessToken } from "../services/authServices";
 import { registerSchema, loginSchema } from "../validators/authValidators";
+import { asyncHandler } from "../utils/asyncHandler";
 
-export const register = async (req: Request, res: Response, next: NextFunction) => {
-            try {
+export const register = asyncHandler(
+            async (req: Request, res: Response, next: NextFunction) => {
+
                         const { error } = registerSchema.validate(req.body);
-                        if (error) return res.status(400).json({ message: error.message });
+                        if (error) {
+                                    res.status(400).json({ message: error.message });
+                                    return
+                        }
 
                         const { name, email, password } = req.body;
                         const user = await registerUser(name, email, password);
 
                         res.status(201).json({ message: "User Registered", user });
-            } catch (err) {
-                        next(err);
-            }
-};
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-            try {
+            }
+)
+
+export const login = asyncHandler(
+            async (req: Request, res: Response) => {
+
                         const { error } = loginSchema.validate(req.body);
-                        if (error) return res.status(400).json({ message: error.message });
+                        if (error) {
+                                    res.status(400).json({ message: error.message });
+                                    return
+                        }
 
                         const { email, password } = req.body;
                         const { user, accessToken, refreshToken } = await loginUser(email, password);
@@ -33,21 +41,22 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
                         });
 
                         res.status(200).json({ message: "Login Success", accessToken, user });
-            } catch (err) {
-                        next(err);
-            }
-};
 
-export const refresh = async (req: Request, res: Response, next: NextFunction) => {
-            try {
+            }
+)
+
+export const refresh = asyncHandler(
+            async (req: Request, res: Response) => {
+
                         const token = req.cookies.refreshToken;
-                        if (!token) return res.status(401).json({ message: "Unauthorized" });
+                        if (!token) {
+                                    res.status(401).json({ message: "Unauthorized" });
+                                    return;
+                        }
 
                         const payload = jwt.verify(token, process.env.JWT_REFRESH_SECRET as string);
                         const accessToken = await refreshAccessToken(payload);
 
                         res.json({ accessToken });
-            } catch (err) {
-                        next(err);
             }
-};
+);
