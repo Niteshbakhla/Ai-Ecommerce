65cd0b1d0a2d144a8c63b17794c7eba0ee8884dc
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-26 21:13:35 +0530

    feat: added a slices and setup the store

---
+++ frontend/src/store/slices/authSclies.ts
@@ -0,0 +1,36 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+
+interface User {
+            _id: string;
+            name: string;
+            email: string;
+            role: string,
+}
+
+interface AuthState {
+            user: User | null;
+            accessToken: string | null;
+}
+
+const initialState: AuthState = {
+            user: null,
+            accessToken: null,
+};
+const authSlice = createSlice({
+            name: "auth",
+            initialState,
+            reducers: { 
+                        setCredentials: (state, action: PayloadAction<{ user: User; acessToken: string }>) => {
+                                    state.user = action.payload.user;
+                                    state.accessToken = action.payload.acessToken;
+                        },
+                        logout: (state) => {
+                                    state.user = null;
+                                    state.accessToken = null;
+                        }
+            }
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+export default authSlice.reducer;
