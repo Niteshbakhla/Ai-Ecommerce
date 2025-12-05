0a782e0b6056a8c74ce39c7c8360318d0d95a745
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-05 20:10:15 +0530

    major update: multiple features and fixes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "@/services/product.service";
 import ProductCard from "@/components/common/ProductCard";
 import { useState } from "react";
+import SearchBar from "@/components/common/SearchBar";
+
 
 export default function Home() {
             const [page, setPage] = useState(1);
@@ -11,18 +13,21 @@ export default function Home() {
                         queryFn: () => getProducts(page),
             });
 
+
             if (isLoading) return <p className="text-center mt-10">Loading products...</p>;
             if (isError) return <p className="text-center mt-10 text-red-500">Failed to load products.</p>;
 
             return (
                         <div>
-                                    <h1 className="text-2xl font-bold mb-4">Featured Products</h1>
+                                    <SearchBar />
 
                                     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
                                                 {data?.products.items?.map((p: any) => (
                                                             <ProductCard key={p._id} product={p} />
                                                 ))}
                                     </div>
+
+
 
                                     {/* Pagination */}
                                     <div className="flex justify-center mt-6 gap-4">
