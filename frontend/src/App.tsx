1f3884329f0bdf12d59e6388fb9ba7b3e6749fc1
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-06 12:51:26 +0530

    chore:imported a admin dasbhoard in app.jsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Layout from "./components/layouts/Layout"
 import Home from "./pages/Home"
 import ProductDetails from "./pages/ProductDetail"
 import CartPage from "./pages/Cart"
+import AdminDashboard from "./pages/admin-dashboard/AdminDashboard"
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/cart" element={<CartPage />} />
+            <Route path="/admin-dashboard" element={<AdminDashboard />} />
 
           </Routes>
         </Layout>
