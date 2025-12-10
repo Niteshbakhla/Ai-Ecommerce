d823098a14f1d9509f8196beaa17cb92eea4f37e
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-10 19:16:07 +0530

    refactor: remove the unnecessary console

--- frontend/src/services/cart.services.ts
+++ frontend/src/services/cart.services.ts
@@ -18,7 +18,6 @@ export const getUserCarts = async () => {
 export const updateCartQuantity = async (itemId: string, quantity: number) => {
             console.log(itemId)
             const res = await API.patch(`/cart/${itemId}`, { quantity });
-            console.log(res)
             return res.data;
 };
 
