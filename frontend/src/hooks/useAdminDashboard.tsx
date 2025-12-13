50d839ff7297d5cb0271179973c84890123c1f65
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-13 11:23:51 +0530

    implemented the cutom hooks

--- frontend/src/hooks/useAdminDashboard.tsx
+++ frontend/src/hooks/useAdminDashboard.tsx
@@ -1,13 +1,47 @@
 import API from "@/api/axiosInstance";
 import { useQuery } from "@tanstack/react-query";
 
-
-export const useAdminOverview = () => {
-            return useQuery({
-                        queryKey: ["admin-overview"],
+export const useAdminOverviewQuery = () =>
+            useQuery({
+                        queryKey: ["admin", "overview"],
                         queryFn: async () => {
                                     const res = await API.get("/admin/analytics/overview");
                                     return res.data;
-                        }
+                        },
             });
-}
+
+export const useAdminSalesQuery = () =>
+            useQuery({
+                        queryKey: ["admin", "sales"],
+                        queryFn: async () => {
+                                    const res = await API.get("/admin/analytics/sales");
+                                    return res.data;
+                        },
+            });
+
+export const useAdminTopProductsQuery = () =>
+            useQuery({
+                        queryKey: ["admin", "top-products"],
+                        queryFn: async () => {
+                                    const res = await API.get("/admin/analytics/top-products");
+                                    return res.data;
+                        },
+            });
+
+export const useAdminInventoryQuery = () =>
+            useQuery({
+                        queryKey: ["admin", "inventory"],
+                        queryFn: async () => {
+                                    const res = await API.get("/admin/analytics/inventory");
+                                    return res.data;
+                        },
+            });
+
+export const useAdminCustomersQuery = () =>
+            useQuery({
+                        queryKey: ["admin", "customers"],
+                        queryFn: async () => {
+                                    const res = await API.get("/admin/analytics/customers");
+                                    return res.data;
+                        },
+            });
