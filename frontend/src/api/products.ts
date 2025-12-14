cac33ad22a06770289fd028ddb158b56b4e94aca
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-14 16:04:22 +0530

    refactor: use correct endpoints

--- frontend/src/api/products.ts
+++ frontend/src/api/products.ts
@@ -1,8 +1,10 @@
-import type { Product } from "@/types/adminTypes";
+import type { ProductFormData } from "@/types/adminTypes";
 import API from "./axiosInstance"
+import type { UpdateProductPayload } from "@/types/productTypes";
 
-export const createProduct = async (productData: Product) => {
-            const { data } = await API.post("/", productData);
+export const createProduct = async (productData: ProductFormData) => {
+
+            const { data } = await API.post("/products", productData);
             return data;
 };
 
@@ -12,3 +14,14 @@ export const deleteProduct = async (id: string) => {
             const { data } = await API.delete(`/products/${id}`)
             return data;
 }
+
+
+
+
+export const updateProduct = async ({
+            id,
+            updateData,
+}: UpdateProductPayload) => {
+            const { data } = await API.patch(`/products/${id}`, updateData);
+            return data;
+};
