7e7a13540b1c36b86b195e681e5ff5501783e222
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-14 16:04:41 +0530

    refactor: remove the unnecessary imports

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -17,7 +17,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { setCredentials } from "@/store/slices/authSlices";
 import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
-import { getNewAccessToken } from "@/services/auth.service";
+
 
 
 export default function Login() {
