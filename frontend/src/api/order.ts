8b7714d448564516fc9455108e2dd8117bae8f63
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-12-17 17:56:27 +0530

    refactor: move files and clean up old structure

--- frontend/src/api/order.ts
+++
@@ -1,9 +0,0 @@
-import API from "./axiosInstance"
-
-
-export const getUserOrder = async () => {
-            const { data } = await API.get("/orders")
-            console.log(data)
-            return data
-
-}
--- frontend/src/services/auth.service.ts
+++
@@ -1,7 +0,0 @@
-import API from "../api/axiosInstance";
-
-export const getNewAccessToken = async () => {
-            const res = await API.post("/auth/refresh");
-            console.log("frontend Response", res.data);
-            return res.data.accessToken;
-};
