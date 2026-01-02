23692da52209dc08f6f54e1d08168c97dc415409
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-02 17:15:27 +0530

    feat: created a new page loader component

---
+++ frontend/src/components/common/PageLoader.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react"
+
+const PageLoader = () => {
+            return (
+                        <div className="flex items-center justify-center h-screen">
+                                    <Loader2 className="h-10 w-10 animate-spin text-blue-600" />
+                        </div>
+            )
+}
+
+export default PageLoader
