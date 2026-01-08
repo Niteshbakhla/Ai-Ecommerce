fc9fd3c3ec5402f8825e283926b64ddfc82604b3
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-08 22:15:10 +0530

    chore: remove unused config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -14,13 +14,6 @@ export default {
             JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES,      // e.g. "15m"
             JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES,    // e.g. "30d"
 
-            CLOUDINARY_URL: process.env.CLOUDINARY_URL,
-            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-
-
-
             RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
             RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
 
@@ -32,8 +25,4 @@ export default {
 
             BASE_URL: process.env.BASE_URL,
 
-            SMTP_HOST: process.env.SMTP_HOST,
-            SMTP_PORT: process.env.SMTP_PORT,
-            SMTP_USER: process.env.SMTP_USER,
-            SMTP_PASS: process.env.SMTP_PASS
 };
