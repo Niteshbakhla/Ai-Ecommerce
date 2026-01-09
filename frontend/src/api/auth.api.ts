49303e2e1320e3bba75d6ab9315615f5c64073d9
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-09 19:24:41 +0530

    chore: remove unnecessary files

--- frontend/src/api/auth.api.ts
+++
@@ -1,6 +0,0 @@
-import API from "./axiosInstance";
-
-export const getNewAccessToken = async () => {
-            const res = await API.post("/auth/refresh");
-            return res.data.accessToken;
-};
