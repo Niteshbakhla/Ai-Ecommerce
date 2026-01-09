2df2e328bb63141ffdb5b29ff939b353e3ca53f6
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-09 19:34:48 +0530

    chore: remove the unused usestate

--- frontend/src/components/layouts/Layout.tsx
+++ frontend/src/components/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useSelector, useDispatch } from "react-redux";
