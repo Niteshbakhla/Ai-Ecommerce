1738b7a7956c906fdf26d8523f51bc4ad8ee6e47
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-09 19:26:05 +0530

    fix: fix the height

--- frontend/src/pages/admin-dashboard/pages/OrderPage.tsx
+++ frontend/src/pages/admin-dashboard/pages/OrderPage.tsx
@@ -114,7 +114,7 @@ export default function OrdersPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
+    <div className=" bg-gray-50 p-6">
       <div className="max-w-7xl mx-auto">
         {/* Header */}
       
