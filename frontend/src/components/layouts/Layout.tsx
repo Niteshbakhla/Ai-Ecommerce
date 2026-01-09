1f03e2b7a22994015b672f0562265fcbf85151e7
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-09 14:45:45 +0530

    refactor: used searchprovider in layout

--- frontend/src/components/layouts/Layout.tsx
+++ frontend/src/components/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useSelector, useDispatch } from "react-redux";
@@ -6,20 +6,19 @@ import { type RootState } from "../../store/store"
 import { logout } from "@/store/slices/authSlices";
 import { useQuery } from "@tanstack/react-query";
 import { getUserCarts } from "@/api/cart.services";
+import { SearchProvider } from "@/SearchContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
-            // Correct TS-safe selector
             const user = useSelector((state: RootState) => state.auth.user);
             const location = useLocation();
 
             const dispatch = useDispatch();
 
-            // For navigation
             const navigate = useNavigate();
 
             const logoutFunction = () => {
-                        dispatch(logout());   // 👈 correct Redux logout
+                        dispatch(logout());
                         navigate("/login");
             };
             const getCurrentPage = (pathname: string): "/" | "login" | "register" | "admin" | undefined => {
@@ -36,11 +35,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             });
 
             return (
-                        <>
+                        <><SearchProvider>
                                     <Navbar
                                                 user={user}
                                                 cartCount={isLoading ? 0 : data?.items?.length ?? 0}
-
                                                 currentPage={getCurrentPage(location.pathname)}
                                                 onLogoClick={() => navigate("/")}
                                                 onCartClick={() => navigate("/cart")}
@@ -50,7 +48,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                                                 onLogout={logoutFunction}
                                     />
 
-                                    <main className="min-h-screen bg-gray-50 p-4">{children}</main>
+                                    <main className="min-h-screen bg-gray-50">{children}</main>
+                        </SearchProvider>
                         </>
             );
 }
